971869342629a238126dd87e51600654be714919
Gregg Tavares <github@greggman.com>
2015-10-31 21:35:21 +0900

    remove bom

--- server/vertexshaderart/lib/common.js
+++ server/vertexshaderart/lib/common.js
@@ -1,4 +1,4 @@
-﻿Art = new Mongo.Collection("art");
+Art = new Mongo.Collection("art");
 ArtRevision = new Mongo.Collection("artrevision"); // note: username show username at time of revision
 ArtLikes = new Mongo.Collection("artlikes");
 
