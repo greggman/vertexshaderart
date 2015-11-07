671d3c95bccb6f41f450e12c46ed812485ef528c
Gregg Tavares <github@greggman.com>
2015-11-07 16:57:59 +0900

    add dev restore/backup

---
+++ server/deploy/backup-dev.sh
@@ -0,0 +1,20 @@
+#!/bin/sh
+set -e
+rm -f  backups/latest-backup.tar.gz
+rm -f  backups/latest-images.tar.gz
+rm -rf backups/latest
+
+echo "dump data"
+mongodump -h 127.0.0.1 --port 3001 --out backups/latest
+tar -s "#backups/latest/meteor#backup/latest/admin#" -s "#backups/latest#backup/latest#" -c -v -z -f backups/latest-backup.tar.gz backups/latest/
+tar -s "#\\.\\./vsa-uploads#data/images#" -c -v -z -f backups/latest-images.tar.gz ../vsa-uploads/
+
+rm -rf backups/latest
+
+TODAY=`date +%Y-%m-%d.%H:%M:%S`
+DST_IMAGES="backups/images-$TODAY-dev.tar.gz"
+DST_DATABASE="backups/backup-$TODAY-dev.tar.gz"
+mv backups/latest-backup.tar.gz $DST_DATABASE
+mv backups/latest-images.tar.gz $DST_IMAGES
+
+
---
+++ server/deploy/restore-dev.sh
@@ -0,0 +1,25 @@
+#!/bin/sh
+if [ -z "$1" ]
+then
+  echo "specify the date of the backup"
+  exit 1
+fi
+set -e
+
+rm -rf backups/latest
+
+DATE="$1"
+SRC_IMAGES="backups/images-$DATE.tar.gz"
+SRC_DATABASE="backups/backup-$DATE.tar.gz"
+cp $SRC_IMAGES   backups/latest-images.tar.gz
+cp $SRC_DATABASE backups/latest-backup.tar.gz
+
+tar -s "#backup/latest/admin#backups/latest/meteor#" -s "#backup/latest#backups/latest#" -x -v -z -f backups/latest-backup.tar.gz
+tar -s "#data/images#vsa-uploads#" -P -x -v -z -f backups/latest-images.tar.gz -C ..
+
+mongorestore -h 127.0.0.1 --port 3001 --drop backups/latest
+
+rm -rf backups/latest
+rm -f  backups/latest-images.tar.gz
+rm -f  backups/latest-backup.tar.gz
+
