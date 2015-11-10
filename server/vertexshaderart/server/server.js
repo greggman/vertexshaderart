9326d8e8887595acb9ac27a62b21dee42038ef2f
Gregg Tavares <github@greggman.com>
2015-11-10 18:50:25 +0900

    remove no-transform test

--- server/vertexshaderart/server/server.js
+++ server/vertexshaderart/server/server.js
@@ -162,13 +162,6 @@ WebApp.connectHandlers.use(function(req, res, next) {
   next();
 });
 
-Meteor.startup(function() {
-  WebApp.rawConnectHandlers.use(function(req, res, next) {
-    if (req.url.substr(0, 7) !== "/static") {
-      res.setHeader('Cache-Control', 'no-transform');
-    }
-    next();
-  });
-});
+
 
 
