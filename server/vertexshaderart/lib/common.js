8f0b9330782df6915b2d7b974104d2703f3e6ff7
Gregg Tavares <github@greggman.com>
2015-12-07 20:08:34 +0900

    add Comments collection

--- server/vertexshaderart/lib/common.js
+++ server/vertexshaderart/lib/common.js
@@ -1,5 +1,6 @@
 Art = new Mongo.Collection("art");
-ArtRevision = new Mongo.Collection("artrevision"); // note: username show username at time of revision
+ArtRevision = new Mongo.Collection("artrevision"); // note: username is the username at time of revision
 ArtLikes = new Mongo.Collection("artlikes");
+Comments = new Mongo.Collection("comments");
 
 
