88ee7901453d718e5b0661752975ea572a4e1b09
Gregg Tavares <github@greggman.com>
2015-12-31 00:50:00 +0900

    fix restore to live

--- server/deploy/restore-live.sh
+++ server/deploy/restore-live.sh
@@ -1,5 +1,5 @@
 #!/bin/sh
 DOCKER=vertexshaderart.com
 DATE="$1"
-bash scripts/restore-support.sh $DOCKER "$DATE" "$DOCKER/backup"
+bash scripts/restore-support.sh $DOCKER "$DATE" "$DOCKER:backup"
 
